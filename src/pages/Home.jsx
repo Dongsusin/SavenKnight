1025ecb1af5ad010a962ee94fd8f4768591f7628
Dongsusin <auroratime020715>
2025-06-21 17:02:16 +0900

    홈페이지 섹션분리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,28 @@
-import { useState, useEffect } from "react";
 import "./Home.css";
 
 export default function Home() {
+  return (
+    <div className="home-page">
+
+      <div className="home-content">
+        {/* 왼쪽 섹션 */}
+        <div className="home-section left-section">
+          <h3>업데이트</h3>
+          <ul className="update-list">
+            <li><a href="https://game.naver.com/lounge/sena_rebirth/board/detail/6184591" target="_blank">[2025-06-12] 업데이트 내역</a></li>
+            <li><a href="https://game.naver.com/lounge/sena_rebirth/board/detail/6103024" target="_blank">[2025-05-29] 업데이트 내역</a></li>
+            <li><a href="https://game.naver.com/lounge/sena_rebirth/board/detail/6056683" target="_blank">[2025-05-21] 업데이트 내역</a></li>
+          </ul>
+        </div>
+
+        {/* 중앙 섹션*/}
+
+        {/* 오른쪽 섹션 */}
+        <div className="home-section right-section">
+          <h3>공지사항</h3>
+          <p>현재 공지사항이 없습니다.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
